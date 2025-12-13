d8c4028e1fcef8b6a8c7e3b6f7c55742d6418c44
ysek0615 <y.sekcal@gmail.com>
2025-12-13 23:51:33 +0900

    refactor: update SortButton to use PrimaryButton

--- frontend/src/app/tasks/components/SortButton.tsx
+++ frontend/src/app/tasks/components/SortButton.tsx
@@ -1,21 +1,23 @@
 import React from "react";
+import { TriggerButtonProps, TriggerButtonRef } from "@/app/tasks/type";
+import TriggerButton from "@/app/tasks/components/ui/TriggerButton";
 import { SortIcon } from "@/app/tasks/components/icons";
 
-const SortButton = React.forwardRef<
-  HTMLButtonElement,
-  React.ButtonHTMLAttributes<HTMLButtonElement>
->(function SortButton(props, ref) {
-  return (
-    <button
-      ref={ref}
-      type="button"
-      className="hover:bg-muted flex cursor-pointer items-center rounded-md transition-colors hover:opacity-80 focus:outline-none"
-      {...props}
-    >
-      <SortIcon />
-      ソート
-    </button>
-  );
-});
+const SortButton = React.forwardRef<TriggerButtonRef, TriggerButtonProps>(
+  function SortButton(props, ref) {
+    return (
+      <TriggerButton
+        ref={ref}
+        {...props}
+        className="hover:bg-muted flex cursor-pointer items-center rounded-md transition-colors hover:opacity-80 focus:outline-none"
+        {...props}
+      >
+        ソート
+        <SortIcon />
+      </TriggerButton>
+    );
+  },
+);
 
+SortButton.displayName = "SortButton";
 export default SortButton;
